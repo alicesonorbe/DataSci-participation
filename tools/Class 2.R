5188a81e2674c1b6411bb436301a781d8f56b2fe
alicesonorbe <59966517+alicesonorbe@users.noreply.github.com>
2020-01-23 11:45:00 -0500

    reorganized

---
+++ tools/Class 2.R
@@ -0,0 +1,13 @@
+install.packages("lsr")
+this_year <- 2020
+birth_year <- 2001
+my_age <- this_year - birth_year
+my_age
+2020%%4
+2020%%100
+2020%%400
+leapyears2000_2050 <- c(2000, 2004, 2008, 2012, 2016, 2020, 2024, 2028, 2032, 2036, 2040, 2044, 2048) 
+leapyears2000_2050 [6]
+length (leapyears2000_2050)
+  sqrt (7744) == 88
+  
