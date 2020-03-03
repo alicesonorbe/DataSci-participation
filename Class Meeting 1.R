14a91c2c1d982501f471c16bcfd8969e88c35e71
alicesonorbe <59966517+alicesonorbe@users.noreply.github.com>
2020-03-03 10:18:17 -0500

    Update!!

---
+++ Class Meeting 1.R
@@ -0,0 +1,102 @@
+2 + 2
+3+4
+
+number <- 3
+number
+number * 2
+
+number <- 5
+number * 2
+
+number
+
+another_number 
+
+times <- c(17, 30, 25, 35, 25, 30, 40, 20)
+times
+mean (times)
+
+times / 60
+
+time_hours <- times / 60
+
+mean (times)
+range (times)
+sqrt (times)
+
+times > 30
+times == 17
+which(times > 30)
+all (times > 30)
+any (times > 30)
+
+?
+?any
+
+?mean
+
+times [times > 30]
+times [3]
+times [!3]
+times [-3]
+3:5
+times [3:5]
+times [c(2, 4)]
+times [-c(2, 4)]
+times > 30
+
+times [times>30]
+
+times
+times [1] <-  47
+times
+times [times > 30] <- NA
+times [1] <-  47
+times
+
+mean (times)
+mean (times,na.rm = TRUE)
+
+# mean of the values between 20 and 35
+times > 20 & times > 35 
+times [times > 20 & times < 35 ] ,na.rm = TRUE
+
+mean(times,na.rm = TRUE)
+mean(times, trim = .2, na.rm = TRUE)
+
+
+mtcars
+?mtcars
+head(mtcars)
+tail(mtcars)
+str(mtcars)
+names (mtcars)
+
+practicee <- c(7.45934, 8.0732, 6.9533, 6.2216, 7.46081)
+round (practicee, 3)
+round (practicee, 4)
+install.packages("swirl")
+library("swirl")
+swirl () 
+Aliceson
+3
+1
+1
+1
+5+7
+x<-5+7
+x
+y<-x-3
+y
+z <- c(1.1,9,3.14)
+?c
+z
+c(z,555,z)
+z*2+100
+my_sqrt <- sqrt(z-1) 
+Aliceson
+1
+my_sqrt
+my_div <- z/my_sqrt
+1
+my_div
